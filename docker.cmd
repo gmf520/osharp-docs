79a6cb064d4de88305dc898de1188ddc6ec92e12
郭明锋 <gmf520@yeah.net>
2019-04-21 21:35:39 +0800

    添加“快速上手”和“设计详解”的文档菜单列表

--- docker.cmd
+++ docker.cmd
@@ -1,2 +1,4 @@
 :: http://127.0.0.1:4205
-docker run --rm -it -p 4205:8000 -v /d/WorkSpace/Source/Repos/osharp-docs:/docs squidfunk/mkdocs-material
+docker run --name osharp-docs --rm -it -p 4205:8000 -v /d/WorkSpace/Source/Repos/osharp-docs:/docs squidfunk/mkdocs-material
+
+:: docker run --rm -it -v /d/WorkSpace/Source/Repos/osharp-docs:/docs squidfunk/mkdocs-material build
